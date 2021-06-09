531d66578bb033a8d608734a01123350c077348a
name-chichi <namechichi123@gmail.com>
2021-06-09 21:21:59 +0900

    django_jquery_210609

--- 02_django/js/js/views.py
+++ 02_django/js/js/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def loginimpl(request):
+    id = request.POST['id']
+    pwd = request.POST['pwd']
+    print(id, pwd)
+    return render(request, 'jq04.html')
+
+
+def registerimpl(request):
+    id = request.POST['id']
+    pwd = request.POST['pwd']
+    name = request.POST['name']
+    age = request.POST['age']
+    print(id, pwd, name, age)
+    return render(request, 'jq05.html')
