6bac43fdaf0b0be94996f2fab65f59916d500395
name-chichi <namechichi123@gmail.com>
2021-07-30 12:09:54 +0900

    Update KDT

--- 02_django/iot/iot.py
+++ 02_django/iot/iot.py
@@ -5,7 +5,7 @@ from urllib import request
 import bs4 as bs4
 
 while True:
-    url = 'http://127.0.0.1/iots?'
+    url = 'http://127.0.0.1:8000/iots?'
     speed = str(random.randint(1, 200))
     rpm = str(random.randint(0, 3000))
     temp = str(random.randint(0, 100))
@@ -13,7 +13,7 @@ while True:
     url += 'rpm='+rpm+'&'
     url += 'temp='+temp
     target = request.urlopen(url)
-    result = bs4.BeautifulSoup(target,'html.parser')
+    result = bs4.BeautifulSoup(target, 'html.parser')
     if result.select_one('h1').string != 'OK':
         break
     time.sleep(3)
